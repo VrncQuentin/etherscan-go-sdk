456153e762831bdfe702e11880d4090e457b6d3d
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2021-08-18 00:00:21 +0200

    update(module/account): balance now set target on creation of calls

--- account/balance/batch.go
+++ account/balance/batch.go
@@ -1,76 +1,75 @@
 package balance
 
 import (
-    "encoding/json"
-    "fmt"
-    "polygonscan/account"
-    "polygonscan/base"
+	"encoding/json"
+	"fmt"
+	"polygonscan/account"
+	"polygonscan/base"
 )
 
 // Doc: https://polygonscan.com/apis#accounts
 // TODO: Find out what `tag` represent in the query & whether it is useful for the wrap
 
 type (
-    BatchAccount struct {
-        *base.Call
-    }
+	BatchAccount struct {
+		*base.Call
+	}
 
-    batchResult struct {
-        base.CallResult
-        Result []accountBalance `json:"result"`
-    }
+	batchResult struct {
+		base.CallResult
+		Result []accountBalance `json:"result"`
+	}
 
-    accountBalance struct {
-        Account string `json:"account"`
-        Balance string `json:"balance"`
-    }
+	accountBalance struct {
+		Account string `json:"account"`
+		Balance string `json:"balance"`
+	}
 )
 
 const (
-    // Batch requests are limited to 20 accounts, according to polygonscan's API.
-    maxAccountPerBatch = 20
+	// Batch requests are limited to 20 accounts, according to polygonscan's API.
+	maxAccountPerBatch = 20
 )
 
-
 func NewBatchAccount(token string) *BatchAccount {
-    return &BatchAccount{
-        base.NewCall(token, new(batchResult)),
-    }
+	tx := &BatchAccount{
+		base.NewCall(token, new(batchResult)),
+	}
+	tx.SetTarget(account.ModuleName, account.BalanceBatch)
+	return tx
 }
 
 func (tx *BatchAccount) Result() []accountBalance {
-    return tx.Res.(*batchResult).Result
+	return tx.Res.(*batchResult).Result
 }
 
 // Get gets the balance of multiple addresses in a single call
 // Up to 20 addresses per batch.
 func (tx *BatchAccount) Get(addresses ...string) (*BatchAccount, error) {
-    if len(addresses) > maxAccountPerBatch {
-        return nil, fmt.Errorf("too many account requested (%d > %d)", len(addresses), maxAccountPerBatch)
-    }
+	if len(addresses) > maxAccountPerBatch {
+		return nil, fmt.Errorf("too many account requested (%d > %d)", len(addresses), maxAccountPerBatch)
+	}
 
-    return tx.get(addresses), nil
+	return tx.get(addresses), nil
 }
 
 // MustGet gets the balance of multiple addresses in a single call
 // Up to 20 addresses per batch, if given more will ignore all the extra addresses.
 func (tx *BatchAccount) MustGet(addresses ...string) *BatchAccount {
-    if len(addresses) > maxAccountPerBatch {
-        addresses = addresses[:maxAccountPerBatch]
-    }
+	if len(addresses) > maxAccountPerBatch {
+		addresses = addresses[:maxAccountPerBatch]
+	}
 
-    return tx.get(addresses)
+	return tx.get(addresses)
 }
 
 func (tx *BatchAccount) get(addresses []string) *BatchAccount {
-    tx.SetTarget(account.ModuleName, account.BalanceBatch).
-        SetAddresses(addresses)
-
-    return tx
+	tx.SetAddresses(addresses)
+	return tx
 }
 
 // Unmarshal converts bytes to a batchResult.
 // Allows batchResult to implement base.Result
 func (r *batchResult) Unmarshal(data []byte) error {
-    return json.Unmarshal(data, r)
+	return json.Unmarshal(data, r)
 }
--- account/balance/single.go
+++ account/balance/single.go
@@ -1,44 +1,44 @@
 package balance
 
 import (
-    "encoding/json"
-    "polygonscan/account"
-    "polygonscan/base"
+	"encoding/json"
+	"polygonscan/account"
+	"polygonscan/base"
 )
 
 // Doc: https://polygonscan.com/apis#accounts
 // TODO: Find out what `tag` represent in the query & whether it is useful for the wrap
 
 type (
-    SingleAccount struct {
-        *base.Call
-    }
+	SingleAccount struct {
+		*base.Call
+	}
 
-    singleResult struct {
-        base.CallResult
-        Balance string `json:"result"`
-    }
+	singleResult struct {
+		base.CallResult
+		Balance string `json:"result"`
+	}
 )
 
 func NewSingleAccount(token string) *SingleAccount {
-    return &SingleAccount{
-        base.NewCall(token, new(singleResult)),
-    }
+	tx := &SingleAccount{
+		base.NewCall(token, new(singleResult)),
+	}
+	tx.SetTarget(account.ModuleName, account.Balance)
+	return tx
 }
 
 func (tx *SingleAccount) Result() string {
-    return tx.Res.(*singleResult).Balance
+	return tx.Res.(*singleResult).Balance
 }
 
 func (tx *SingleAccount) Get(address string) *SingleAccount {
-    tx.SetTarget(account.ModuleName, account.Balance).
-        SetAddress(address)
-
-    return tx
+	tx.SetAddress(address)
+	return tx
 }
 
 // Unmarshal converts bytes to a singleResult.
 // Allows singleResult to implement base.Result
 func (r *singleResult) Unmarshal(data []byte) error {
-    return json.Unmarshal(data, r)
+	return json.Unmarshal(data, r)
 }
