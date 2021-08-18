97ad862e53e95b8033ae3d3f0e02a7d2b9b2c730
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2021-08-18 02:33:15 +0200

    feat(module/contracts): added wrappers around it (abi call doesn't have data definition atm)

--- contracts/abi.go
+++ contracts/abi.go
@@ -1,1 +1,44 @@
 package contracts
+
+import (
+	"encoding/json"
+	"polygonscan/base"
+)
+
+type (
+	ABI struct {
+		*base.Call
+	}
+
+	ABIResult struct {
+		base.CallResult
+		Data ABIData `json:"result"`
+	}
+
+	ABIData struct {
+		//TODO: Add data
+	}
+)
+
+func NewABI(token string) *ABI {
+	tx := &ABI{
+		base.NewCall(token, new(ABIResult)),
+	}
+	tx.SetTarget(ModuleName, GetABI)
+	return tx
+}
+
+func (tx *ABI) Result() ABIData {
+	return tx.Res.(*ABIResult).Data
+}
+
+func (tx *ABI) Get(address string) *ABI {
+	tx.SetContractAddress(address)
+	return tx
+}
+
+// Unmarshal converts bytes to a ABIResult.
+// Allows ABIResult to implement base.Result
+func (r *ABIResult) Unmarshal(data []byte) error {
+	return json.Unmarshal(data, r)
+}
--- contracts/code.go
+++ contracts/code.go
@@ -1,1 +1,56 @@
 package contracts
+
+import (
+	"encoding/json"
+	"polygonscan/base"
+)
+
+type (
+	SourceCode struct {
+		*base.Call
+	}
+
+	SourceCodeResult struct {
+		base.CallResult
+		Data []SourceCodeData `json:"result"`
+	}
+
+	SourceCodeData struct {
+		SourceCode           string `json:"SourceCode"`
+		ABI                  string `json:"ABI"`
+		ContractName         string `json:"ContractName"`
+		CompilerVersion      string `json:"CompilerVersion"`
+		OptimizationUsed     string `json:"OptimizationUsed"`
+		Runs                 string `json:"Runs"`
+		ConstructorArguments string `json:"ConstructorArguments"`
+		EVMVersion           string `json:"EVMVersion"`
+		Library              string `json:"Library"`
+		LicenseType          string `json:"LicenseType"`
+		Proxy                string `json:"Proxy"`
+		Implementation       string `json:"Implementation"`
+		SwarmSource          string `json:"SwarmSource"`
+	}
+)
+
+func NewSourceCode(token string) *SourceCode {
+	tx := &SourceCode{
+		base.NewCall(token, new(SourceCodeResult)),
+	}
+	tx.SetTarget(ModuleName, GetSourceCode)
+	return tx
+}
+
+func (tx *SourceCode) Result() []SourceCodeData {
+	return tx.Res.(*SourceCodeResult).Data
+}
+
+func (tx *SourceCode) Get(address string) *SourceCode {
+	tx.SetContractAddress(address)
+	return tx
+}
+
+// Unmarshal converts bytes to a SourceCodeResult.
+// Allows SourceCodeResult to implement base.Result
+func (r *SourceCodeResult) Unmarshal(data []byte) error {
+	return json.Unmarshal(data, r)
+}
